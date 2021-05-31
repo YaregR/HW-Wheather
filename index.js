4e247af038b9a024d772151987f9e5c61d17210d
Yareg <yaregr@gmail.com>
2021-06-01 00:08:39 +0300

    second

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const url = 'http://api.openweathermap.org/data/2.5/forecast?q=Kyiv&units=metric&appid=d7a9c2aa6483208653ed799e72f4386a';
+const key = 'd7a9c2aa6483208653ed799e72f4386a';
+const id = 703448;
+
+const url = `http://api.openweathermap.org/data/2.5/forecast?id=${id}&units=metric&appid=${key}`;
 
 let timer = setInterval(() => {
     axios.get(url).then((data) => {
-        console.log(data);
-    });
-}, 900000);
-
+        const nextDay = data.data.list[7];
+        const {main, dt_txt} = nextDay;
+        const res = `${dt_txt} = ${main.temp} C, ` + '\n';
+        fs.appendFileSync('file.txt', res, function (err) {
+            if (err) throw err;
+        });
+    });    
+}, 3000);
 
     
 
